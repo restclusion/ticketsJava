6b90a71be74c38b2041c46f9aafc31a7a71eb0c9
Dys <dysismedia@gmail.com>
2021-07-17 16:43:41 +0200

    Correciones Main, entrada por consola, etc

--- ticketsJava/src/ticketsJava/Ticket.java
+++ ticketsJava/src/ticketsJava/Ticket.java
@@ -5,17 +5,29 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Ticket {
+	private String caso;
 	public static void main(String[] args) throws Exception {
+		
+		System.out.println("-------------- Sistema de tickets --------------\n");
+		System.out.println("-------------- ENTRADAS --------------\n");
+		System.out.println("Casos del Enunciado + 1 personal: entrada1, entrada2, entrada3, entrada4 \n");
+		System.out.println("Casos de ERROR (formatos incorrectos): entrada5, entrada6 \n");
+		System.out.println("Elije NUMERO de entrada: {1, 2, 3, 4, 5, 6}\n");
+		Scanner scan = new Scanner(System.in);
+		int s = scan.nextInt();
+		
 		try {
-			FileInputStream fis = new FileInputStream("caso4.txt");
+			FileInputStream fis = new FileInputStream("entrada" + s + ".txt");
 			InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
 			BufferedReader br = new BufferedReader(isr);
 
 			String linea;
 			Producto prod;
 			Carrito carrito = new Carrito();
+			System.out.println("-----------------");
 
 			while ((linea = br.readLine()) != null)   {		
 				prod = carrito.transformarProducto(linea);
